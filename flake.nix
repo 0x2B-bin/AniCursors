0ecb70888eb0e77929a146328f2124d761f11c67
0x2B <nhagan345@gmail.com>
2026-08-15 12:32:05 -0400

    Removed flake-parts

--- flake.nix
+++ flake.nix
@@ -3,34 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-
-      perSystem = { pkgs, lib, self', ... }:
-        let
-          mkCursor = pkgs.callPackage ./packages/mkCursor.nix {};
-          cursorNames = builtins.attrNames (builtins.readDir ./src);
-
-          cursorSet = lib.genAttrs cursorNames (name: mkCursor { inherit name; });
-        in
-        {
-          packages = cursorSet // {
-            all = pkgs.symlinkJoin {
-              name = "anicursors-all";
-              paths = builtins.attrValues cursorSet;
-            };
-
-            default = self'.packages.all;
+  outputs = inputs: {
+    packages = builtins.mapAttrs (
+      system: pkgs:
+      let
+        mkCursor = pkgs.callPackage ./packages/mkCursor.nix { };
+        cursorNames = builtins.attrNames (builtins.readDir ./src);
+        cursorSet = pkgs.lib.genAttrs cursorNames (name: mkCursor { inherit name; });
+      in
+      (
+        args@{ ... }:
+        args
+        // {
+          all = pkgs.symlinkJoin {
+            name = "anicursors-all";
+            paths = builtins.attrValues cursorSet;
           };
-        };
-    };
+          default = inputs.self.packages.${system}.all;
+        }
+      )
+        cursorSet
+    ) inputs.nixpkgs.legacyPackages;
+  };
 }
